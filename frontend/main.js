84e23f64ac4d524050ed97c00d70e13c19170e79
CatDave23 <david.iannece@gandhimerano.com>
2023-10-06 16:41:57 +0200

    aggiunto lavoro frontend

---
+++ frontend/main.js
@@ -0,0 +1,30 @@
+/*$.ajax({
+  import example from "../galleria/example.json";
+  dataType: "json",
+  success: function (example) {
+
+      $.each(example, function(i,path) {
+          $('#galleria').prepend(' <div class = "image fade"><img class="img1" src="'+ "../galleria/files/" + path +'"></div>');
+      });
+  }
+});*/
+
+import jsonData from "../galleria/example.json";
+console.log(jsonData);
+
+let index = 0;
+displayImages();
+function displayImages() {
+  let i;
+  const images = document.getElementsByClassName("image");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  index++;
+  if (index > images.length) {
+    index = 1;
+  }
+  images[index-1].style.display = "block";
+  setTimeout(displayImages, 2000); 
+}
+
