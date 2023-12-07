a1a562ab5d2f34b326b652fbb39ee221fe32b18b
nkoexe <niccolo.rag@gmail.com>
2023-12-07 15:57:14 +0100

    dio

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -3,6 +3,18 @@ socket = io('/frontend');
 // Marquee dove inserire le notizie
 const notizie = document.getElementById('notizie').children[0];
 
+const dio = document.getElementById('dio');
+
+function esci_da_schermo_intero() {
+    document.exitFullscreen();
+}
+
+document.onkeydown = (event) => {
+    if (event.code === 'Escape') {
+        dio.classList.toggle('expanded');
+    }
+}
+
 socket.on('biblioteca', (data) => {
     console.log(data)
 })
@@ -17,7 +29,7 @@ socket.on('notizie', (data) => {
     for (const id in data) {
 
         if (data[id].active) {
-            notizie.innerHTML += data[id].text
+            notizie.innerHTML += data[id].text + "&emsp;&emsp;"
         }
     }
 })
