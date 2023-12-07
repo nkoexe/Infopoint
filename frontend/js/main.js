be0cbbdda37eb8181942122e0317c28d7403b6c4
nkoexe <niccolo.rag@gmail.com>
2023-12-07 13:56:47 +0100

    upload test notizie

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,5 +1,8 @@
 socket = io('/frontend');
 
+// Marquee dove inserire le notizie
+const notizie = document.getElementById('notizie').children[0];
+
 socket.on('biblioteca', (data) => {
     console.log(data)
 })
@@ -9,5 +12,12 @@ socket.on('galleria', (data) => {
 })
 
 socket.on('notizie', (data) => {
-    console.log(data)
+    notizie.innerHTML = ''
+
+    for (const id in data) {
+
+        if (data[id].active) {
+            notizie.innerHTML += data[id].text
+        }
+    }
 })
