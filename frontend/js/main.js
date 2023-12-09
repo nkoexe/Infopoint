6decb453335a252002a9d36dd7dcc616c7a7126c
nkoexe <niccolo.rag@gmail.com>
2023-12-09 01:25:56 +0100

    galleria appost ma da ottimizzare

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,7 +1,6 @@
-const template_youtube = `<iframe id="galleria_youtube" src="{src}" type="text/html" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`
-const template_video = `<video id="galleria_video" autoplay controls loop><source src="{src}" type="video/mp4"></video>`
-const template_immagine = `<img id="galleria_immagine" src="{src}" alt="Qui ci dovrebbe essere un'immagine. Se stai leggendo questo testo, contatta <>" />
-`
+// const template_youtube = `<iframe id="galleria_youtube" src="{src}" type="text/html" frameborder="0" allow="autoplay; encrypted-media" sandbox="allow-same-origin allow-scripts" allowfullscreen></iframe>`
+const template_video = `<video id="galleria_video" onended="cambia_elemento_galleria()" autoplay controls><source src="{src}" type="video/mp4"></video>`
+const template_immagine = `<img id="galleria_immagine" src="{src}" alt="Qui ci dovrebbe essere un'immagine. Se stai leggendo questo testo, contatta <>" />`
 
 socket = io('/frontend');
 
@@ -16,14 +15,31 @@ const titolobiblioteca = document.getElementById('titolobiblioteca');
 const immaginebiblioteca = document.getElementById('immaginebiblioteca');
 const descrizionebiblioteca = document.getElementById('descrizionebiblioteca');
 
-let index_galleria = 0;
-let dati_galleria;
+let index_galleria = -1;
+let dati_galleria = [];
 const elemento_galleria = document.getElementById('elemento_galleria');
 const galleria_didascalia = document.getElementById('didascaliagalleria').children[0];
 
 
-function esci_da_schermo_intero() {
-    document.exitFullscreen();
+function toggleFullScreen() {
+    if (!document.fullscreenElement &&
+        !document.mozFullScreenElement && !document.webkitFullscreenElement) {
+        if (document.documentElement.requestFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.documentElement.mozRequestFullScreen) {
+            document.documentElement.mozRequestFullScreen();
+        } else if (document.documentElement.webkitRequestFullscreen) {
+            document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
+        }
+    } else {
+        if (document.cancelFullScreen) {
+            document.cancelFullScreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitCancelFullScreen) {
+            document.webkitCancelFullScreen();
+        }
+    }
 }
 
 document.onkeydown = (event) => {
@@ -41,12 +57,32 @@ function cambia_elemento_galleria() {
     let elemento = dati_galleria[index_galleria];
 
     if (elemento.type === 'youtube') {
-        console.log(elemento.path)
 
-        elemento_galleria.innerHTML = template_youtube.replace("{src}", elemento.path + "?autoplay=1&controls=0&disablekb=1&enablejsapi=1&fs=0&hl=it&loop=1&modestbranding=1&iv_load_policy=3")
-        elemento_galleria.children[0].setAttribute("allow", "autoplay");
-        elemento_galleria.children[0].play();
+        // elemento_galleria.innerHTML = template_youtube.replace("{src}", elemento.path + "?autoplay=1&controls=0&disablekb=1&enablejsapi=1&fs=0&hl=it&loop=1&modestbranding=1&iv_load_policy=3")
+        elemento_galleria.innerHTML = '<div id="galleria_youtube"></div>'
         galleria_didascalia.innerHTML = elemento.text;
+
+        new YT.Player('galleria_youtube', {
+            videoId: elemento.path,
+            playerVars: {
+                modestbranding: 1,
+                controls: 0,
+                disablekb: 1,
+                fs: 0,
+                hl: 'it',
+                iv_load_policy: 3
+            },
+            events: {
+                'onReady': (event) => {
+                    event.target.playVideo();
+                },
+                'onStateChange': (event) => {
+                    if (event.data == YT.PlayerState.ENDED) {
+                        cambia_elemento_galleria();
+                    }
+                }
+            }
+        });
 
     } else if (elemento.type === 'video') {
         elemento_galleria.innerHTML = template_video.replace("{src}", "galleria/" + elemento.path)
@@ -55,10 +91,20 @@ function cambia_elemento_galleria() {
     } else if (elemento.type === 'image') {
         elemento_galleria.innerHTML = template_immagine.replace("{src}", "galleria/" + elemento.path)
         galleria_didascalia.innerHTML = elemento.text;
+
+        setTimeout(() => {
+            cambia_elemento_galleria();
+        }, 5000)
+
+    } else {
+        console.log("Errore nel tipo di elemento: " + elemento.type)
+        cambia_elemento_galleria();
     }
 };
 
-setInterval(cambia_elemento_galleria, 5000);
+
+
+// -------------------------------
 
 
 
@@ -77,6 +123,8 @@ socket.on('galleria', (data) => {
             dati_galleria.push(data[elemento]);
         }
     }
+
+    cambia_elemento_galleria();
 })
 
 socket.on('notizie', (data) => {
