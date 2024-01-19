64d5a1469462fe1157087406c4969a8712423ac2
nkoexe <niccolo.rag@gmail.com>
2024-01-19 18:59:45 +0100

    boh

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from flask_socketio import SocketIO
 import logging
 
@@ -8,6 +8,11 @@ app = Flask(__name__)
 app.secret_key = b"0ee6f27b79730fb025949c4d792f084adadcf4796bfdeb980c6ec1abf1fd7a70"
 
 socketio = SocketIO(app)
+
+
+@app.route("/")
+def index():
+    return redirect(url_for("backend.index"))
 
 
 from routes import backend
