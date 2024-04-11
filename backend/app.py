039bb232301b2755506ea8ebcd9d8eb288b6eb64
nkoexe <niccolo.rag@gmail.com>
2024-04-11 11:06:46 +0200

    cache dei file

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,8 @@ logger = logging.getLogger(__name__)
 app = Flask(__name__)
 app.secret_key = b"0ee6f27b79730fb025949c4d792f084adadcf4796bfdeb980c6ec1abf1fd7a70"
 
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 24 * 60 * 60
+
 socketio = SocketIO(app)
 
 
